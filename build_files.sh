22515d125a8e004559ea2a8a0f2f5ca082f307f4
SomendraSaini <somendra2406@outlook.com>
2024-11-09 14:33:16 +0530

    editied for vercel

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+# Install pip if not already installed
+if ! command -v pip &> /dev/null
+then
+    echo "pip could not be found, installing pip..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py
+fi
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput
